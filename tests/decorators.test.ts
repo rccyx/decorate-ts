f8bef9716453a5eae9021a662641f97da15b9e72
ashgw <ashrefgw@proton.me>
2025-08-21 22:30:28 +0100

    add preliminary tests

--- tests/decorators.test.ts
+++ tests/decorators.test.ts
@@ -1,91 +1,160 @@
-import { describe, it, expect } from "vitest";
-import {
-  Final,
-  Frozen,
-  Sealed,
-  Singleton,
-  FinalTypeError,
-} from "../src/index.js";
+import { Final, Frozen, FinalTypeError } from '../src';
+import { test, expect } from 'vitest';
 
-describe("Decorators", () => {
-  describe("@Final", () => {
-    it("should prevent inheritance", () => {
-      @Final
-      class TestClass {
-        value = "test";
-      }
+test('Should not allow inheritance of a final frozen class. "FinalTypeError" should be thrown', () => {
+  @Final
+  @Frozen
+  class Foo<T> {
+    private _foo: T;
+    bar: string;
 
-      class SubClass extends TestClass {
-        subValue = "sub";
-      }
+    constructor(foo: T) {
+      this._foo = foo;
+      this.bar = 'bar';
+    }
+    someFoo(): T {
+      return this._foo;
+    }
+  }
 
-      expect(() => new TestClass()).not.toThrow();
-      expect(() => new SubClass()).toThrow(FinalTypeError);
-    });
-  });
-
-  describe("@Frozen", () => {
-    it("should freeze instance properties", () => {
-      @Frozen
-      class TestClass {
-        value = "initial";
-
-        constructor(val?: string) {
-          if (val) this.value = val;
-        }
-      }
-
-      const instance = new TestClass("test");
-      expect(instance.value).toBe("test");
-
-      expect(() => {
-        instance.value = "modified";
-      }).toThrow();
-    });
-  });
-
-  describe("@Sealed", () => {
-    it("should seal instance properties", () => {
-      @Sealed
-      class TestClass {
-        value = "initial";
-
-        constructor(val?: string) {
-          if (val) this.value = val;
-        }
-      }
-
-      const instance = new TestClass("test");
-      expect(instance.value).toBe("test");
-
-      // Should allow modification of existing properties
-      instance.value = "modified";
-      expect(instance.value).toBe("modified");
-
-      // Should prevent adding new properties
-      expect(() => {
-        (instance as any).newProp = "new";
-      }).toThrow();
-    });
-  });
-
-  describe("@Singleton", () => {
-    it("should ensure only one instance exists", () => {
-      @Singleton
-      class TestClass {
-        value: string;
-
-        constructor(val: string) {
-          this.value = val;
-        }
-      }
-
-      const instance1 = new TestClass("first");
-      const instance2 = new TestClass("second");
-
-      expect(instance1).toBe(instance2);
-      expect(instance1.value).toBe("first");
-      expect(instance2.value).toBe("first");
-    });
-  });
+  class SubFoo extends Foo<string> {
+    constructor(foo: string) {
+      super(foo);
+    }
+  }
+  expect(() => {
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    const _ = new SubFoo('subbedFoo');
+  }).toThrowError(FinalTypeError);
 });
+
+test('Should allow instantiation of a final frozen class with no problems', () => {
+  @Final
+  @Frozen
+  class Foo<T> {
+    private _foo: T;
+    bar: string;
+
+    constructor(foo: T) {
+      this._foo = foo;
+      this.bar = 'bar';
+    }
+    someFoo(): T {
+      return this._foo;
+    }
+  }
+  expect(() => {
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    const _ = new Foo('foo');
+  }).not.toThrow();
+});
+
+test('Should bot allow to alter  attributes of the final frozen class, throws TypeError since it is frozen', () => {
+  @Final
+  @Frozen
+  class Foo<T> {
+    private _foo: T;
+    bar: string;
+
+    constructor(foo: T) {
+      this._foo = foo;
+      this.bar = 'bar';
+    }
+    someFoo(): T {
+      return this._foo;
+    }
+  }
+  expect(() => {
+    const foo = new Foo('foo');
+    foo.bar = 'altered';
+  }).toThrow(TypeError);
+});
+
+test('Should work when the final sealed class is a subclass itself, TypeError should be thrown', () => {
+  abstract class BaseFoo<T> {
+    abstract someFoo(): T;
+  }
+  @Final
+  @Frozen
+  class Foo<T> extends BaseFoo<T> {
+    private _foo: T;
+    bar: string;
+
+    constructor(foo: T) {
+      super();
+      this._foo = foo;
+      this.bar = 'bar';
+    }
+    someFoo(): T {
+      return this._foo;
+    }
+  }
+  expect(() => {
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    const _ = new Foo('foo').bar;
+  }).not.toThrow(TypeError);
+});
+
+test(`Should not allow inheritance, of the final class, when the final class
+  is a subclass itself, a TypeError should be thrown`, () => {
+  abstract class BaseFoo<T> {
+    abstract someFoo(): T;
+  }
+  @Final
+  @Frozen
+  class Foo<T> extends BaseFoo<T> {
+    private _foo: T;
+    bar: string;
+
+    constructor(foo: T) {
+      super();
+      this._foo = foo;
+      this.bar = 'bar';
+    }
+    someFoo(): T {
+      return this._foo;
+    }
+  }
+
+  class SubFoo extends Foo<string> {
+    constructor(foo: string) {
+      super(foo);
+    }
+  }
+  expect(() => {
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    const _ = new SubFoo('foo').bar;
+  }).toThrow(FinalTypeError);
+});
+
+test(`One limitation, the @Final decorator should be on top of @Frozen not the opposite, otherwise FinalTypeError would be thrown upon instantitation of the final class`, () => {
+  abstract class BaseFoo<T> {
+    abstract someFoo(): T;
+  }
+  @Frozen
+  @Final
+  class Foo<T> extends BaseFoo<T> {
+    private _foo: T;
+    bar: string;
+
+    constructor(foo: T) {
+      super();
+      this._foo = foo;
+      this.bar = 'bar';
+    }
+    someFoo(): T {
+      return this._foo;
+    }
+  }
+
+  class SubFoo extends Foo<string> {
+    constructor(foo: string) {
+      super(foo);
+    }
+  }
+  expect(() => {
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    const _ = new Foo('foo');
+  }).toThrow(FinalTypeError);
+});
+
